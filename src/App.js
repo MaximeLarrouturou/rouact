0aa9e706eef83380147bdac8486ae5a259051a5c
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2019-07-26 14:36:51 +0200

    Components Todos

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+  state = {
+    todos: []
+  }
+
+componentDidMount() {
+  this.getTodos()
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        todos: data
+      })
+    });
+}
+
+getTodos() {
+  return fetch('https://jsonplaceholder.typicode.com/todos')
+}
+
+render() {
+  console.log(this.state.todos);
+  return (
+    <div className="App">
+      <header className="App-header">
+       le routage
+      </header>
+      <Todos  todos={this.state.todos}/>
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Todo from './Todo';
+
+const Todos = ({todos}) => {
+  return (
+    <div>
+      <ul className="todos">
+      {todos && todos.map(todo => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos; 
