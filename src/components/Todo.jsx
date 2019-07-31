63ff70f8621321e9c69294758ad4fae7b5466767
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2019-07-31 11:20:56 +0200

    Route avec des paramétres dynamiques

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Todo = ({todo}) => {
   return (
-    <li className="todo">{todo.title}</li>
+    <li className="todo">{todo.title}
+        <br></br>
+          <Link to={`/todos/${todo.id}`}>details…</Link>
+    </li>
+
   );
 }
 
---
+++ src/components/TodoDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TodoDetails = (props) => {
+    return ( 
+        <h2>Vous êtes bien sur la page TodoDetails</h2>  
+     );
+     
+}
+
+export default TodoDetails;
